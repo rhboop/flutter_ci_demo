3200bb30405f8e4a03d89b3040a079318d62d35a
Ivan <srhsyk@gmail.com>
2020-06-19 14:53:24 +0900

    Add firebase function.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_performance/firebase_performance.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+void main() {
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+  runZoned(() {
+    runApp(MyApp());
+  }, onError: Crashlytics.instance.recordError);
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -24,7 +38,103 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class _MetricHttpClient extends BaseClient {
+  _MetricHttpClient(this._inner);
+
+  final Client _inner;
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) async {
+    final HttpMetric metric = FirebasePerformance.instance
+        .newHttpMetric(request.url.toString(), HttpMethod.Get);
+
+    await metric.start();
+    StreamedResponse response;
+    try {
+      response = await _inner.send(request);
+      metric
+        ..responsePayloadSize = response.contentLength
+        ..responseContentType = response.headers['Content-Type']
+        ..requestPayloadSize = request.contentLength
+        ..httpResponseCode = response.statusCode;
+    } finally {
+      await metric.stop();
+    }
+
+    return response;
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  FirebasePerformance _performance = FirebasePerformance.instance;
+  bool _isPerformanceCollectionEnabled = false;
+  String _performanceCollectionMessage =
+      'Unknown status of performance collection.';
+  bool _traceHasRan = false;
+  bool _httpMetricHasRan = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _togglePerformanceCollection();
+  }
+
+  Future<void> _togglePerformanceCollection() async {
+    await _performance
+        .setPerformanceCollectionEnabled(!_isPerformanceCollectionEnabled);
+
+    final bool isEnabled = await _performance.isPerformanceCollectionEnabled();
+    setState(() {
+      _isPerformanceCollectionEnabled = isEnabled;
+      _performanceCollectionMessage = _isPerformanceCollectionEnabled
+          ? 'Performance collection is enabled.'
+          : 'Performance collection is disabled.';
+    });
+  }
+
+  Future<void> _testTrace() async {
+    setState(() {
+      _traceHasRan = false;
+    });
+
+    final Trace trace = _performance.newTrace("test");
+    trace.incrementMetric("metric1", 16);
+    trace.putAttribute("favorite_color", "blue");
+
+    await trace.start();
+
+    int sum = 0;
+    for (int i = 0; i < 10000000; i++) {
+      sum += i;
+    }
+    print(sum);
+
+    await trace.stop();
+
+    setState(() {
+      _traceHasRan = true;
+    });
+  }
+
+  Future<void> _testHttpMetric() async {
+    setState(() {
+      _httpMetricHasRan = false;
+    });
+
+    final _MetricHttpClient metricHttpClient = _MetricHttpClient(Client());
+
+    final Request request = Request(
+      "SEND",
+      Uri.parse("https://www.google.com"),
+    );
+
+    metricHttpClient.send(request);
+
+    setState(() {
+      _httpMetricHasRan = true;
+    });
+  }
+
   int _counter = 0;
 
   void _incrementCounter() {
@@ -35,6 +145,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final TextStyle textStyle =
+        const TextStyle(color: Colors.lightGreenAccent, fontSize: 25.0);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -43,6 +155,74 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text(_performanceCollectionMessage),
+            RaisedButton(
+              onPressed: _togglePerformanceCollection,
+              child: const Text('Toggle Data Collection'),
+            ),
+            RaisedButton(
+              onPressed: _testTrace,
+              child: const Text('Run Trace'),
+            ),
+            Text(
+              _traceHasRan ? 'Trace Ran!' : '',
+              style: textStyle,
+            ),
+            RaisedButton(
+              onPressed: _testHttpMetric,
+              child: const Text('Run HttpMetric'),
+            ),
+            Text(
+              _httpMetricHasRan ? 'HttpMetric Ran!' : '',
+              style: textStyle,
+            ),
+            FlatButton(
+                child: const Text('Key'),
+                onPressed: () {
+                  Crashlytics.instance.setString('foo', 'bar');
+                }),
+            FlatButton(
+                child: const Text('Log'),
+                onPressed: () {
+                  Crashlytics.instance.log('baz');
+                }),
+            FlatButton(
+                child: const Text('Crash'),
+                onPressed: () {
+                  // Use Crashlytics to throw an error. Use this for
+                  // confirmation that errors are being correctly reported.
+                  Crashlytics.instance.crash();
+                }),
+            FlatButton(
+                child: const Text('Throw Error'),
+                onPressed: () {
+                  // Example of thrown error, it will be caught and sent to
+                  // Crashlytics.
+                  throw StateError('Uncaught error thrown by app.');
+                }),
+            FlatButton(
+                child: const Text('Async out of bounds'),
+                onPressed: () {
+                  // Example of an exception that does not get caught
+                  // by `FlutterError.onError` but is caught by the `onError` handler of
+                  // `runZoned`.
+                  Future<void>.delayed(const Duration(seconds: 2), () {
+                    final List<int> list = <int>[];
+                    print(list[100]);
+                  });
+                }),
+            FlatButton(
+                child: const Text('Record Error'),
+                onPressed: () {
+                  try {
+                    throw 'error_example';
+                  } catch (e, s) {
+                    // "context" will append the word "thrown" in the
+                    // Crashlytics console.
+                    Crashlytics.instance
+                        .recordError(e, s, context: 'as an example');
+                  }
+                }),
             Text(
               'You have pushed the button this many times:',
             ),
@@ -57,7 +237,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
